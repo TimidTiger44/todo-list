b36558150979edf26244a383a45fef47d9902748
Darrell <183925276+TimidTiger44@users.noreply.github.com>
2026-08-31 19:00:26 -0400

    add methods to todo objects

--- src/todo.js
+++ src/todo.js
@@ -1,11 +1,35 @@
+import { addDays, isAfter, isWithinInterval } from 'date-fns';
+
 function createTodo(title, description, dueDate, priority) {
     let todoPriority = priority;
+    let completed = false;
+
+    const toggleCompleted = () => {
+        completed = !completed;
+    }
 
     const changePriority = (newPriority) => {
         if (["low", "medium", "high"].includes(newPriority)){
             todoPriority = newPriority;
         }
     }
+
+    const isPassedDue = () => {
+        // create date-fns object with current date
+        let currentDate = new Date();
+
+        return isAfter(currentDate, dueDate);
+    }
+
+    const isDueThisWeek = () => {
+        const currentDate = new Date();
+        const weekFromNow = addDays(currentDate, 7);
+
+        return isWithinInterval(dueDate, {
+            start: currentDate,
+            end: weekFromNow
+        });
+    };
 
     return {
         title,
