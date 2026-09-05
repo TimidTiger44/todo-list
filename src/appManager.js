1719043eeca691ac195edd38c643c8cc30170853
Darrell <183925276+TimidTiger44@users.noreply.github.com>
2026-09-05 10:43:24 -0400

    add local storage for projects. Still need to add logic for capturing todo list items. Difficult to conceptualize as-is because each to-do is attached to a project. But for now, local storage works for projects.

--- src/appManager.js
+++ src/appManager.js
@@ -7,32 +7,47 @@ function appManager() {
 // initialze projectManager object
 const manager = projectManager();
 // create default project to add initial todos to
-const defaultProject = createProject("Default", "General to-do's go here.");
-manager.addProject(defaultProject);
 
-let currentProject = defaultProject;
+// helper function to save typing
+const save = () => {
+    localStorage.setItem("projects", JSON.stringify(manager.getProjects()));
+    };
+
+const savedProjects = localStorage.getItem("projects");
+
+if (savedProjects) {
+    const parsedProjects = JSON.parse(savedProjects);
+    parsedProjects.forEach(p => {
+        const project = createProject(p.name, p.description);
+        manager.addProject(project);
+    });
+} else {
+    const defaultProject = createProject("Default", "General to-do's go here.");
+    manager.addProject(defaultProject);
+    save();
+}
+
+
+let currentProject = manager.getProjects()[0];
 
 const addNewProject = (name, description) => {
     const newProject = createProject(name, description);
     manager.addProject(newProject);
+    save();
 };
 
 const addTodoToCurrentProject = (name, description, dueDate, priority) => {
     const todoToAdd = createTodo(name, description, dueDate, priority);
     currentProject.addTodoItem(todoToAdd);
-}
+    save();
+};
 
 const setCurrentProject = (project) => {
     currentProject = project;
 };
 
-const getCurrentProject = () => {
-    return currentProject;
-};
-
-const getAllProjects = () => {
-    return manager.getProjects();
-};
+const getCurrentProject = () => currentProject;
+const getAllProjects = () => manager.getProjects();
 
 return {
     addNewProject,
